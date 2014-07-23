dde7d67720429ba0c741a17618e2730c185a24cb
Peter Pilgrim <peter.pilgrim@gmail.com>
2014-07-23 23:43:48 +0100

    Chapter 5 - JSF Instant Secure Credit - added conversational scope bean and page flow.

---
+++ ch05/jsf-instant-secure/src/main/java/uk/co/xenonique/digital/instant/LendingController.java
@@ -0,0 +1,55 @@
+package uk.co.xenonique.digital.instant;
+
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+/**
+ * The type LendingController
+ *
+ * @author Peter Pilgrim
+ */
+
+@Named("lendingController")
+@ConversationScoped
+public class LendingController implements Serializable {
+
+    @Inject private Conversation    conversation;
+
+    public void start() {
+        if ( conversation.isTransient()) {
+            conversation.begin();
+        }
+    }
+
+    public void end() {
+        if (!conversation.isTransient()) {
+            conversation.end();
+        }
+    }
+
+    public String gotoStart() {
+        start();
+        return "getting-started?faces-redirect=true";
+    }
+
+    public String doGettingStarted() {
+        start();
+        return "your-details?faces-redirect=true";
+    }
+
+    public String doYourDetails() {
+        return "your-rate?faces-redirect=true";
+    }
+
+    public String doYourRate() {
+        return "completion?faces-redirect=true";
+    }
+
+    public String doCompletion() {
+        end();
+        return "index?faces-redirect=true";
+    }
+}
