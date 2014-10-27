f5ac7b6ee26552db797df347ce2901ecf57fd635
Peter Pilgrim <peter.pilgrim@gmail.com>
2014-10-27 23:32:33 +0000

    ch06: Work in progress

---
+++ ch06/jsf-product-flow/src/main/java/uk/co/xenonique/digital/instant/control/LoginController.java
@@ -0,0 +1,12 @@
+package uk.co.xenonique.digital.instant.control;
+
+import javax.inject.Named;
+
+/**
+ * The type LoginController
+ *
+ * @author Peter Pilgrim
+ */
+@Named
+public class LoginController {
+}
