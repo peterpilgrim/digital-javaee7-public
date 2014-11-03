bb6f6be6c568e0ff4f6dc5dd41c17bae5fd0b94e
Peter Pilgrim <peter.pilgrim@gmail.com>
2014-11-03 23:24:07 +0000

    Ch06: Product flow stage 2 - ability to create a campaign #2 data population - create and edit campaign complete - remove todo

--- ch06/jsf-product-flow-stage2/src/main/java/uk/co/xenonique/digital/product/control/CampaignController.java
+++ ch06/jsf-product-flow-stage2/src/main/java/uk/co/xenonique/digital/product/control/CampaignController.java
@@ -5,6 +5,7 @@ import uk.co.xenonique.digital.product.boundary.UserProfileService;
 import uk.co.xenonique.digital.product.entity.Campaign;
 import uk.co.xenonique.digital.product.entity.UserProfile;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.flow.FlowScoped;
 import javax.inject.Inject;
@@ -23,6 +24,7 @@ import static uk.co.xenonique.digital.product.utils.AppConsts.*;
 @FlowScoped("campaign")
 public class CampaignController implements Serializable {
 
+    private int id;
     private Campaign  campaign;
 
     @Inject
@@ -45,16 +47,49 @@ public class CampaignController implements Serializable {
 
     // Actions
 
-    public String createCampaign() {
+    private void updateCreationUser() {
         String userKey =  (String)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(LOGIN_KEY);
         List<UserProfile> users = userService.findByUsername(userKey);
         campaign.setCreationUser(users.get(0));
+    }
+
+    public String createCampaign() {
+        updateCreationUser();
         campaignService.add(campaign);
         campaign = new Campaign();
         return "campaign.xthml?faces-redirect=true";
     }
 
+    public void retrieveById() {
+        if (id <= 0) {
+            String message = "Bad request. Please use a link from within the system.";
+            FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+            return;
+        }
+        List<Campaign> campaigns = campaignService.findById(id);
+        this.campaign = campaigns.get(0);
+    }
+
+    public String editCampaign() {
+        updateCreationUser();
+        campaignService.update(campaign);
+        campaign = new Campaign();
+        return "campaign.xthml?faces-redirect=true";
+    }
+
+    public String removeCampaign() {
+        updateCreationUser();
+        campaignService.delete(campaign);
+        campaign = new Campaign();
+        return "campaign.xthml?faces-redirect=true";
+    }
+
     public String cancel() {
+        return "campaign.xhtml?faces-redirect=true";
+    }
+
+    public String finish() {
         return "/campaign-return.xhtml?faces-redirect=true";
     }
 
@@ -65,6 +100,15 @@ public class CampaignController implements Serializable {
 
 
     // Getter and setters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public Campaign getCampaign() {
         return campaign;
