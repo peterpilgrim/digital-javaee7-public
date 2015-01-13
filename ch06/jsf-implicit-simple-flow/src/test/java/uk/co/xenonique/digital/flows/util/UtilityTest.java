f3c9756610bc427000ac9eb74ccd7c2f0d9a4918
Peter Pilgrim <peter.pilgrim@gmail.com>
2015-01-13 23:36:49 +0000

    Ch06: snapshot #1 Added implicit flow definition example for a gentle introduction.

---
+++ ch06/jsf-implicit-simple-flow/src/test/java/uk/co/xenonique/digital/flows/util/UtilityTest.java
@@ -0,0 +1,15 @@
+package uk.co.xenonique.digital.instant.util;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.math.BigDecimal;
+
+public class UtilityTest {
+
+    @Test
+    public void calculateMonthlyPayment() {
+    }
+
+}
+
