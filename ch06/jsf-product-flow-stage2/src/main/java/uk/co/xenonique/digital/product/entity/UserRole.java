565c9e5a927c39f6415d0bb6cc56c55a0a11e310
Peter Pilgrim <peter.pilgrim@gmail.com>
2015-02-26 17:53:43 +0000

    Ch06: snapshot #9 back to WildFly. Code quality. FINALLY CALLING TIME GETTING HIBERNATE TO WORK AGAINST PAYARA / GLASSFISH 4.1. THIS STUFF SHOULD BE A LOT EASIER.

--- ch06/jsf-product-flow-stage2/src/main/java/uk/co/xenonique/digital/product/entity/UserRole.java
+++ ch06/jsf-product-flow-stage2/src/main/java/uk/co/xenonique/digital/product/entity/UserRole.java
@@ -21,7 +21,7 @@ import java.io.Serializable;
 public class UserRole implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     private String name;
 
