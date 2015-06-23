3297b9063468c96e2b62cf7103df4bbb97960c22
Peter Pilgrim <peter.pilgrim@gmail.com>
2015-06-23 23:46:32 +0100

    Ch09: Revision for the Handlebars Java example #2

--- ch09/handlebars-javaee-mvc/src/main/java/uk/co/xenonique/basic/mvc/FormErrorMessage.java
+++ ch09/handlebars-javaee-mvc/src/main/java/uk/co/xenonique/basic/mvc/FormErrorMessage.java
@@ -39,4 +39,35 @@ public class FormErrorMessage {
     public void setMessage(String message) {
         this.message = message;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FormErrorMessage)) return false;
+
+        FormErrorMessage that = (FormErrorMessage) o;
+
+        if (property != null ? !property.equals(that.property) : that.property != null) return false;
+        if (value != null ? !value.equals(that.value) : that.value != null) return false;
+        return !(message != null ? !message.equals(that.message) : that.message != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = property != null ? property.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        result = 31 * result + (message != null ? message.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("FormErrorMessage{");
+        sb.append("property='").append(property).append('\'');
+        sb.append(", value=").append(value);
+        sb.append(", message='").append(message).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
