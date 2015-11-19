06ef3d63dd29e586079edcfb071b4f3ed5cf79d9
Peter Pilgrim <peter.pilgrim@gmail.com>
2015-11-19 01:08:09 +0000

    Add underscorejs example project with Twitter bootstrap, JQuery example

--- ch07/underscorejs-example/src/main/webapp/js/lib/require-setup.js
+++ ch07/underscorejs-example/src/main/webapp/js/lib/require-setup.js
@@ -9,6 +9,7 @@ var require = {
     // This feature is called back FALLBACK paths. Just in case, we library did not exist, then we could go to a CDN
     paths: {
         "jquery" : [ "jquery-2.1.3.min", "jquery-2.1.3" ],
-        "bootstrap" : [ "bootstrap-3.2.0.min", "bootstrap-3.2.0" ]
+        "bootstrap" : [ "bootstrap-3.2.0.min", "bootstrap-3.2.0" ],
+	    "underscore" : [ "underscore-1.8.2.min", "underscore-1.8.2" ]
     }
 };
